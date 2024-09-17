3745246c31577bffab170bd4c6f55248e624d255
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-09-17 17:31:47 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Full Speed Ahead
+
+This mod removes the parry slowdown. Maybe it has side-effects, who knows tbh.
+
+# EUPL
+                      Copyright (c) 2024 SFGrenade
+                      Licensed under the EUPL-1.2
+https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
---
+++ src/FullSpeedAhead.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Modding;
+using SFCore.Utils;
+
+namespace FullSpeedAhead;
+
+public class FullSpeedAheadMod : Mod
+{
+    public FullSpeedAheadMod() : base("Full Speed Ahead")
+    {
+    }
+
+    public override string GetVersion() => Util.GetVersion(Assembly.GetExecutingAssembly());
+
+    public override void Initialize()
+    {
+        On.GameManager.FreezeMoment_int += (orig, self, type) =>
+        {
+            // type == 0 is `hero death anim` in `dream map zone`
+            // type == 1 is gate clink? bridge/boss lever hit? dung death effect? kill freeze, hit by spikes/acid
+            // type == 2 is ???
+            // type == 3 is parry / nail_clash_tink
+            // type == 4 is stun effect
+            // type == 5 is ???
+            if (type != 3)
+            {
+                orig(self, type);
+            }
+        };
+    }
+}
